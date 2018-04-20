630f676a7294062180966e55c3b5abf60f2c7cf4
ryancooper7 <ryan.cooper7@gmail.com>
2018-04-20 12:50:44 -0600

    Scrapes base depth from the internet for A-Basin

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
+import re
+import requests
+
+def scrape():
+    l = []
+    url = 'https://www.arapahoebasin.com/snow-conditions/'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    for elem in soup(text=re.compile('Base')):
+        if len(elem) == 4:
+            return elem.parent.findPrevious('div', {"class":"ab-condition_value"}).text
 
 app = Flask(__name__)
 
 @app.route('/<string:index>/', methods=['GET','POST'])
 def my_form_post(index):
-    return render_template('%s.html' % index)
+    basin_base = scrape()
+    return render_template('%s.html' % index, base=basin_base)
 
 if __name__ == "__main__":
     app.run(debug=True)
