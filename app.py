91cca8b717c8458fd98a707457a566f742ae748f
ryancooper7 <ryan.cooper7@gmail.com>
2018-04-22 17:07:13 -0600

    Changed Breckenridge to Loveland since those are the only resorts currently open. Scraped Loveland base

--- app.py
+++ app.py
@@ -3,26 +3,38 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-def scrape():
+def scrape_loveland_base():
+    l = []
+    url = 'http://skiloveland.com/snow-report/'
+    r=requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    base = soup.find('span', {'class' : 'mid-mtn-total'})
+    return base.text
+
+
+
+def scrape_abasin_base():
     l = []
     url = 'https://www.arapahoebasin.com/snow-conditions/'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     for elem in soup(text=re.compile('Base')):
-    	print("test \n")
+        print("test \n")
 
-    	print(elem)
-    	print("test \n")
+        print(elem)
+        print("test \n")
 
-    	if len(elem) == 4:
+        if len(elem) == 4:
             return elem.parent.findPrevious('div', {"class":"ab-condition_value"}).text
 
 app = Flask(__name__)
 
 @app.route('/<string:index>/', methods=['GET','POST'])
 def my_form_post(index):
-    basin_base = scrape()
-    return render_template('%s.html' % index, base=basin_base)
+    basin_base = scrape_abasin_base()
+    loveland_base = scrape_loveland_base()
+    print loveland_base
+    return render_template('%s.html' % index, aBayBase=basin_base, lovelandBase = loveland_base)
 
 if __name__ == "__main__":
     app.run(debug=True)
