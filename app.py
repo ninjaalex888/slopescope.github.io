8484a0a3f5d67c4b3f4b4897ce2fd0cdd56a667f
ryancooper7 <ryan.cooper7@gmail.com>
2018-04-23 01:46:45 -0600

    Implemented mapquest api for driving times. Currently hardcoded for boulder

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
 import re
 import requests
+import json
+
+def get_driving_times():
+    body = json.dumps({"locations": ["Boulder, CO", "28194 U.S. 6, Keystone, CO 80435"]})
+    r = requests.post("http://www.mapquestapi.com/directions/v2/routematrix?key=ind9QjhJrKLehF3GGrIoF4UnUtUw14xm", data=body)
+    output=json.loads(r.content)
+    time = output['time'][1]
+    basin_hours = time / 3600
+    basin_minutes = (time % 3600) / 60
+    basin_seconds = time % 60
+
+    return basin_hours, basin_minutes
+
 
 def scrape_loveland():
     l = []
@@ -47,10 +60,13 @@ app = Flask(__name__)
 
 @app.route('/<string:index>/', methods=['GET','POST'])
 def my_form_post(index):
+    basin_hours, basin_minutes = get_driving_times()
     basin_base, basin_twenty_four, basin_seventy_two = scrape_abasin()
     loveland_base, loveland_twenty_four, loveland_seventy_two = scrape_loveland()
-    return render_template('%s.html' % index, aBayBase=basin_base, lovelandBase = loveland_base, lovelandTwentyFour = loveland_twenty_four,
-                           lovelandSeventyTwo = loveland_seventy_two, aBasinTwentyFour = basin_twenty_four, aBasinSeventyTwo = basin_seventy_two)
+    return render_template('%s.html' % index, aBayBase=basin_base, lovelandBase = loveland_base,
+                           lovelandTwentyFour = loveland_twenty_four,lovelandSeventyTwo = loveland_seventy_two,
+                           aBasinTwentyFour = basin_twenty_four, aBasinSeventyTwo = basin_seventy_two,
+                           basinHours = basin_hours, basinMinutes = basin_minutes)
 
 
 if __name__ == "__main__":
